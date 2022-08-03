c9b18be47f36734df414d67f68feb474f007444c
Triet_Luu <s3594528@rmit.edu.vn>
2022-08-03 09:03:31 +0700

    Initial Commit

---
+++ Casino/CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasinoApp.swift
+//  Casino
+//
+//  Created by Triet, Luu Huynh on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
