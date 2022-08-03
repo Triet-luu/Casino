570fc329595c8c0b9a1cc8dd345d85e7f5e29e16
Triet_Luu <s3594528@rmit.edu.vn>
2022-08-03 10:30:58 +0700

    dice casino UI

--- Casino/CasinoTable.swift
+++ Casino/CasinoTable.swift
@@ -10,12 +10,39 @@ import SwiftUI
 struct CasinoTable: View {
     var body: some View {
         ZStack {
-            Image("Welcome_Casino")
-                .resizable()
-                .padding()
-                .frame(width: 200, height: 200)
-            Spacer()
-            
+            VStack {
+                Image("Welcome_Casino")
+                    .resizable()
+                    .padding()
+                    .frame(width: 200, height: 200)
+                HStack {
+                    Image("dice-red-1")
+                        .resizable()
+                        .padding()
+                        .frame(width: 100, height: 100)
+                    Image("dice-red-2")
+                        .resizable()
+                        .padding()
+                        .frame(width: 100, height: 100)
+                }
+                VStack {
+                    Text("VS")
+                        .font(.system(size: 30))
+                        .foregroundColor(.blue)
+                        .background(.red).frame(width: 40, height: 40)
+                    HStack {
+                        Image("dice-blue-4")
+                            .resizable()
+                            .padding()
+                            .frame(width: 100, height: 100)
+                        Image("dice-blue-5")
+                            .resizable()
+                            .padding()
+                            .frame(width: 100, height: 100)
+                    }
+                }
+                Spacer()
+            }
         }
     }
 }
